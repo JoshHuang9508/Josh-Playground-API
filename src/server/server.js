e9cb76314472103aa5f80223f8b48614bf82033c
Whydog <142924636+JoshHuang9508@users.noreply.github.com>
2025-01-05 20:43:47 +0800

    2024-01-05 更新日誌3
    - 優化WebSocket推送邏輯，減少客戶端運算量
    - 加入新增播放清單功能
    - 加入隨機播放功能

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,10 @@ const { Server } = require("socket.io");
 const http = require("http");
 const url = require("url");
 const ytdl = require("ytdl-core");
+const ytpl = require("ytpl");
+
+// Config
+const logLimit = 20;
 
 let playerState = {
   playing: false,
@@ -12,6 +16,7 @@ let playerState = {
   duration: 0,
   playbackRate: 1,
   loop: false,
+  random: false,
   trackQueue: [],
   index: 0,
 };
@@ -56,6 +61,29 @@ const server = http.createServer(async (req, res) => {
     return;
   }
 
+  if (req.url.startsWith("/api/ytpl")) {
+    const query = url.parse(req.url, true).query;
+    const { playlistId } = query;
+
+    if (!playlistId) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "缺少 playlistId 參數" }));
+      return;
+    }
+
+    try {
+      const info = await ytpl(
+        `https://www.youtube.com/playlist?list=${playlistId}`
+      );
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(info.items.filter((item) => item.isPlayable)));
+    } catch (error) {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "無法獲取影片資訊" }));
+    }
+    return;
+  }
+
   res.writeHead(404, { "Content-Type": "application/json" });
   res.end(JSON.stringify({ error: "找不到該路由" }));
 });
@@ -70,13 +98,9 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
-  io.to(socket.id).emit("receiveLog", logs);
-  io.to(socket.id).emit("receiveUsers", users);
-  io.to(socket.id).emit("receivePlayerState", playerState);
-
   socket.on("disconnect", () => {
     console.log("A user disconnected:", socket.id);
-    logs = [`${users[socket.id]} 離開了房間`, ...logs].slice(0, 100);
+    logs = [`${users[socket.id]} 離開了房間`, ...logs].slice(0, logLimit);
     if (users[socket.id]) delete users[socket.id];
 
     io.emit("receiveLog", logs);
@@ -84,11 +108,15 @@ io.on("connection", (socket) => {
   });
 
   socket.on("join", (username) => {
-    logs = [`${username} 加入了房間`, ...logs].slice(0, 100);
+    logs = [`${username} 加入了房間`, ...logs].slice(0, logLimit);
     users[socket.id] = username;
 
     io.emit("receiveLog", logs);
     io.emit("receiveUsers", users);
+  });
+
+  socket.on("ready", () => {
+    io.to(socket.id).emit("receivePlayerState", playerState);
   });
 
   socket.on("setUsername", (username) => {
@@ -96,12 +124,8 @@ io.on("connection", (socket) => {
     io.emit("receiveUsers", users);
   });
 
-  socket.on("setLog", (newLogs) => {
-    logs = newLogs.slice(0, 100);
-    io.emit("receiveLog", logs);
-  });
   socket.on("addLog", (newLog) => {
-    logs = [newLog, ...logs].slice(0, 100);
+    logs = [newLog, ...logs].slice(0, logLimit);
     io.emit("receiveLog", logs);
   });
 
@@ -109,13 +133,79 @@ io.on("connection", (socket) => {
     playerState = { ...playerState, ...state };
     io.emit("receivePlayerState", playerState);
   });
-  socket.on("getPlayerState", () => {
-    io.to(socket.id).emit("getPlayerState", playerState);
-  });
   socket.on("updatePlayerState", (state) => {
     playerState = { ...playerState, ...state };
   });
-
+  socket.on("play", () => {
+    playerState.playing = true;
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("pause", () => {
+    playerState.playing = false;
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("refresh", async () => {
+    playerState.playing = false;
+    io.emit("receivePlayerState", playerState);
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    io.emit("seek", playerState.playedSeconds);
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    playerState.playing = true;
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("addTrack", (track) => {
+    playerState.trackQueue.push(track);
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("addTracks", (tracks) => {
+    playerState.trackQueue.push(...tracks);
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("removeTrack", (index) => {
+    playerState.trackQueue.splice(index, 1);
+    if (index > playerState.index) playerState.index--;
+    playerState.index = Math.min(
+      playerState.index,
+      playerState.trackQueue.length - 1
+    );
+    playerState.index = Math.max(playerState.index, 0);
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("setTrackQueue", (trackQueue) => {
+    playerState.trackQueue = trackQueue;
+    playerState.index = 0;
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("nextTrack", () => {
+    playerState.index =
+      playerState.index == playerState.trackQueue.length - 1
+        ? 0
+        : playerState.index + 1;
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("prevTrack", () => {
+    playerState.index =
+      playerState.index == 0
+        ? playerState.trackQueue.length - 1
+        : playerState.index - 1;
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("setTrackIndex", (index) => {
+    playerState.index = index;
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("setPlaybackRate", (rate) => {
+    playerState.playbackRate = rate;
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("setLoop", (loop) => {
+    playerState.loop = loop;
+    io.emit("receivePlayerState", playerState);
+  });
+  socket.on("setRandom", (random) => {
+    playerState.random = random;
+    io.emit("receivePlayerState", playerState);
+  });
   socket.on("seek", (time) => {
     io.emit("seek", time);
   });
